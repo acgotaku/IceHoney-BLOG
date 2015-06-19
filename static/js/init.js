30ad2a9144c91d29f7ecd3ad2d5c79bbd68e64e4
acgotaku <acgotaku311@gmail.com>
2015-06-19 22:34:47 +0800

    for mobile

--- static/js/init.js
+++ static/js/init.js
@@ -4,15 +4,18 @@
         $("pre, code").addClass("language-javascript");
         $(".page").addClass("hidden");
         $( window ).scroll(function(){
-        	if(window.scrollY>100){
-        		$("nav.top-nav").addClass("fixed");
-        		$("main").addClass("scroll");
-        		$(".page").removeClass("hidden");
-        	}else{
-        		$("nav.top-nav").removeClass("fixed");
-        		$("main").removeClass("scroll");
-        		$(".page").addClass("hidden");
-        	}
+            if(window.innerWidth >923){
+                if(window.scrollY>100){
+                    $("nav.top-nav").addClass("fixed");
+                    $("main").addClass("scroll");
+                    $(".page").removeClass("hidden");
+                }else{
+                    $("nav.top-nav").removeClass("fixed");
+                    $("main").removeClass("scroll");
+                    $(".page").addClass("hidden");
+                }               
+            }
+
         });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
