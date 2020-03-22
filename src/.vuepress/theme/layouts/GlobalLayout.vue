08222e8ce04485317856b94f41ecba545688f17d
acgotaku <acgotaku311@gmail.com>
2020-03-22 21:52:03 +0900

    add global layout

---
+++ src/.vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="global-layout">
+    <header style="background-color: #DDD">
+      <router-link to="/">{{ $site.title }}</router-link>· <router-link to="/tag/">Tag</router-link>
+    </header><br>
+    <DefaultGlobalLayout/><br>
+    <footer style="background-color: #DDD"><a href="https://github.com/ulivz/70-lines-of-vuepress-blog-theme">Github</a> · Powered by VuePress</p>
+</footer>
+  </div>
+</template>
+
+<script>
+import GlobalLayout from "@app/components/GlobalLayout.vue";
+
+export default {
+  components: { DefaultGlobalLayout: GlobalLayout }
+};
+</script>
